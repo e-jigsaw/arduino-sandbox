f3c0e9a79f73969a8761841e6c0b4b3b7f2f6ec0
jigsaw <m@jgs.me>
2018-10-06 15:22:11 +0900

    Arrrray

--- sketch_oct06a/sketch_oct06a.ino
+++ sketch_oct06a/sketch_oct06a.ino
@@ -1,44 +1,36 @@
 #include "Keyboard.h"
 
-const int inputPin1 = 3;
-const int inputPin2 = 4;
+const int keyNum = 2;
+const int inputPin[keyNum] = {3, 4};
 const int ledPin = 13;
-bool currentState1 = LOW;
-bool beforeState1 = LOW;
-bool currentState2 = LOW;
-bool beforeState2 = LOW;
+const byte keyMap[keyNum] = {0x61, 0x62};
+bool currentState[keyNum];
+bool beforeState[keyNum];
+int i;
 
 void setup() {
-  pinMode(inputPin1, INPUT);
-  pinMode(inputPin2, INPUT);
+  for(i = 0; i < keyNum; i++) {
+    pinMode(inputPin[i], INPUT);
+    currentState[i] = LOW;
+    beforeState[i] = LOW;
+  }
   pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
   Keyboard.begin();
 }
 
 void loop() {
-  currentState1 = digitalRead(inputPin1);
-  currentState2 = digitalRead(inputPin2);
-  if (currentState1 != beforeState1) {
-    if (currentState1 == HIGH) {
-      digitalWrite(ledPin, HIGH);
-      Serial.println("PUSH 1");
-//      Keyboard.press(0x61);
-    } else {
-      digitalWrite(ledPin, LOW);
-      Serial.println("RELEASE 1");
-//      Keyboard.release(0x61);
+  for(i = 0; i < keyNum; i++) {
+    currentState[i] = digitalRead(inputPin[i]);
+    if (currentState[i] != beforeState[i]) {
+      Serial.print("KEY");
+      Serial.print(i);
+      if (currentState[i] == HIGH) {
+        Serial.println("PUSH");
+      } else {
+        Serial.println("RELEASE");
+      }
+      beforeState[i] = currentState[i];
     }
-    beforeState1 = currentState1;
-  }
-  if (currentState2 != beforeState2) {
-    if (currentState2 == HIGH) {
-      digitalWrite(ledPin, HIGH);
-      Serial.println("PUSH 2");
-    } else {
-      digitalWrite(ledPin, HIGH);
-      Serial.println("RELEASE 2");
-    }
-    beforeState2 = currentState2;
   }
 }
