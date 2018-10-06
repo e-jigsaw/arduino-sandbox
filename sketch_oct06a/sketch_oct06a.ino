edecb2c2009fd8f4426b84509542d9f03e39003a
jigsaw <m@jgs.me>
2018-10-06 14:49:23 +0900

    Serial

--- sketch_oct06a/sketch_oct06a.ino
+++ sketch_oct06a/sketch_oct06a.ino
@@ -2,19 +2,28 @@
 
 const int inputPin = 3;
 const int ledPin = 13;
+bool currentState = LOW;
+bool beforeState = LOW;
 
 void setup() {
   pinMode(inputPin, INPUT);
   pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
   Keyboard.begin();
 }
 
 void loop() {
-  if (digitalRead(inputPin) == HIGH) {
-    digitalWrite(ledPin, HIGH);
-    Keyboard.press(0x61);
-  } else {
-    digitalWrite(ledPin, LOW);
-    Keyboard.release(0x61);
+  currentState = digitalRead(inputPin);
+  if (currentState != beforeState) {
+    if (digitalRead(inputPin) == HIGH) {
+      digitalWrite(ledPin, HIGH);
+      Serial.println("PUSH");
+//      Keyboard.press(0x61);
+    } else {
+      digitalWrite(ledPin, LOW);
+      Serial.println("RELEASE");
+//      Keyboard.release(0x61);
+    }
+    beforeState = currentState;
   }
 }
